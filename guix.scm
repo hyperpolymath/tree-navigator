513af9d0cdfd1cab44b421831d032c764568c40d
Jonathan D.A. Jewell <6759885+hyperpolymath@users.noreply.github.com>
2026-07-26 16:01:20 +0100

    chore: update guix.scm from squisher-corpus

--- guix.scm
+++ guix.scm
@@ -1,5 +1,5 @@
 ; SPDX-License-Identifier: MPL-2.0
-;; guix.scm — GNU Guix package definition for tree-navigator
+;; guix.scm — GNU Guix package definition for squisher-corpus
 ;; Usage: guix shell -f guix.scm
 
 (use-modules (guix packages)
@@ -7,12 +7,12 @@
              (guix licenses))
 
 (package
-  (name "tree-navigator")
+  (name "squisher-corpus")
   (version "0.1.0")
   (source #f)
   (build-system gnu-build-system)
-  (synopsis "tree-navigator")
-  (description "tree-navigator — part of the hyperpolymath ecosystem.")
-  (home-page "https://github.com/hyperpolymath/tree-navigator")
+  (synopsis "squisher-corpus")
+  (description "squisher-corpus — part of the hyperpolymath ecosystem.")
+  (home-page "https://github.com/hyperpolymath/squisher-corpus")
   (license ((@@ (guix licenses) license) "PMPL-1.0-or-later"
              "https://github.com/hyperpolymath/palimpsest-license")))
